1eca9e9c6fa0298e5b4430708f84db080205be18
pankajdhatrak <71335874+pankajdhatrak@users.noreply.github.com>
2025-04-03 19:25:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT
 
 #convert the snowpark  data frame to pandas
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredient_list = st.multiselect(
     "Choose ingredient"
